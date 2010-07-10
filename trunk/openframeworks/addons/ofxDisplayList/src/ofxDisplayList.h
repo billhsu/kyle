2007b79509515ded5a847964544f8d272767dd2d
kylecimcdonald <kylecimcdonald@a2c67d46-c447-11de-a380-73d83ba7c083>
2010-07-10 03:36:01 +0000

    separated into .h and .cpp

--- trunk/openframeworks/addons/ofxDisplayList/src/ofxDisplayList.h
+++ trunk/openframeworks/addons/ofxDisplayList/src/ofxDisplayList.h
@@ -1,47 +1,21 @@
 #pragma once
 
+#include "ofMain.h"
+
 class ofxDisplayList {
-private:
+protected:
 	GLuint _id;
 	bool _ready;
 	int* _references;
 public:
-	ofxDisplayList() {
-		_id = glGenLists(1);
-		_ready = false;
-		_references = new int;
-		(*_references) = 1;
-	}
-	ofxDisplayList(const ofxDisplayList& list) {
-		_id = list._id;
-		_ready = list._ready;
-		_references = list._references;
-		(*_references)++;
-	}
-	~ofxDisplayList() {
-		(*_references)--;
-		if(*_references == 0) {
-			delete _references;
-			glDeleteLists(_id, 1);
-		}
-	}
-	bool ready() const {
-		return _ready;
-	}
-	void update() {
-		_ready = false;
-	}
-	void begin(bool execute = true) {
-		glNewList(_id, execute ? GL_COMPILE_AND_EXECUTE : GL_COMPILE);
-	}
-	void end() {
-		glEndList();
-		_ready = true;
-	}
-	bool draw() {
-		if(!_ready)
-			return false;
-		glCallList(_id);
-		return true;
-	}
+	ofxDisplayList();
+	ofxDisplayList(const ofxDisplayList& list);
+	~ofxDisplayList();
+	bool ready() const;
+	void update();
+	void begin(bool execute = true);
+	void end();
+	bool draw();
 };
+
+// <3 kyle mcdonald
