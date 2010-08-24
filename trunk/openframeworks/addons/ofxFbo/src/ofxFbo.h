f7e93f6eaeac0d1db02a0118ab6a53143cc5c13c
kylecimcdonald <kylecimcdonald@a2c67d46-c447-11de-a380-73d83ba7c083>
2010-08-24 22:46:54 +0000

    added some notes from discussion with anton

--- trunk/openframeworks/addons/ofxFbo/src/ofxFbo.h
+++ trunk/openframeworks/addons/ofxFbo/src/ofxFbo.h
@@ -4,11 +4,28 @@
 #include "ofGraphics.h"
 
 /*
-	add multisampling support
+	push/pop to be renamed
+
+	// push
+	glGetIntegerv(GL_FRAMEBUFFER_BINDING_EXT, &previousFBO);
+	glGetIntegerv(GL_READ_FRAMEBUFFER_BINDING_EXT, &previousReadFBO);
+	glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING_EXT, &previousDrawFBO);
+
+	// pop 
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, previousFBO);	
+	glBindFramebufferEXT(GL_READ_FRAMEBUFFER_EXT, previousReadFBO);
+	glBindFramebufferEXT(GL_DRAW_FRAMEBUFFER_EXT, previousDrawFBO);
+
+	add warning if you try to use the wrong resolutions
+ 
+	do a glGet to find the current fbo before pushing and popping
+	add multisampling support: 
 	need to be able to load results back to an ofImage maybe using glGetTexImage
 	add all the checks for warnings and errors (just before begin())
 	add OpenGLes support
+
 	should ofFbo override the screen width and screen height returned by ofGetWidth() and ofGetHeight()?
+		ofImageGrabScreen() or ofTextureLoadScreenData() etc.
 */
 
 class ofxFbo {
